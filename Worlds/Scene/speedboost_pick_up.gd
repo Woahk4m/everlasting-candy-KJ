3ec1dfcb80a0b6c64f6cbdc4c708676147ba0e27
Woahk4m <kamilaroj4s3542@hotmail.com>
2025-03-13 17:58:41 -0400

    Implemented mechanics into world with full script

---
+++ Worlds/Scene/speedboost_pick_up.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+# this function is called whenever a body enters into the area2d
+# "body: Node2D" is a reference to the interacted body
+func _on_area_2d_body_entered(body: Node2D):
+	# checks if the body entered is a Player type node
+	if body is Player:
+		# sets the player's speed to twice as fast
+		body.spd = 120.0
+
+		# "get_tree().create_timer(5.0)" creates and starts a timer with 5 seconds
+		# ".timeout.connect(...)" connects a function to the signal "timeout"
+		get_tree().create_timer(5.0).timeout.connect(
+			func():
+				# sets the player's speed back to default
+				body.spd = 60.0
+		)
+
+		# deletes the pickup from the scene
+		queue_free()
